6016396d1ae36805c50673ec7dfe4694eae77d14
NareshBairam <bairamnaresh7@gmail.com>
2018-10-22 15:10:04 +0530

    added signup and validations for signup

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
+var validator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var csrf = require('csurf');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping', { useNewUrlParser: true });
-
+require('./config/passport')
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'hbs');
@@ -23,9 +26,13 @@ app.set('view engine', '.hbs')
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret : "mysupersecret", resave:false, saveUninitialized:false}))
 // app.use(csrf());
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
