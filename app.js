32d22619d7896719215ae4bfd39dbe599571f4da
NareshBairam <bairamnaresh7@gmail.com>
2018-10-23 18:29:49 +0530

    implemented shopping cart view

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var session = require('express-session');
 var csrf = require('csurf');
 var passport = require('passport');
 var flash = require('connect-flash');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -29,7 +30,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret : "mysupersecret", resave:false, saveUninitialized:false}))
+app.use(session({
+  secret: "mysupersecret",
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 // app.use(csrf());
 app.use(flash());
 app.use(passport.initialize());
@@ -38,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
+var Cart = require('../models/cart')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,30 +15,27 @@ router.get('/', function (req, res, next) {
   });
 });
 
-// router.get('/user/signup', function (req, res, next) {
-//   var messages = req.flash('error');
-//   res.render('user/signup', { csrfToken: req.csrfToken(), messages : messages, hasErrors : messages.length > 0 })
-// })
+router.get('/add-to-cart/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {})
 
-// router.post('/user/signup', passport.authenticate('local.signup', {
-//   successRedirect: '/user/profile',
-//   failureRedirect: '/user/signup',
-//   failureFlash: true
-// }));
+  Product.findById(productId, function(err, product){
+    if (err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  })
+});
 
-// router.get('/user/profile', function (req, res, next) {
-//   res.render('user/profile');
-// })
-
-// router.get('/user/signin', function (req, res, next) {
-//   var messages = req.flash('error');
-//   res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 })
-// })
-
-// router.post('/user/signin', passport.authenticate('local.signin', {
-//   successRedirect: '/user/profile',
-//   failureRedirect: '/user/signin',
-//   failureFlash: true
-// }));
+router.get('/shopping-cart', function(req, res, next){
+  if (!req.session.cart){
+    return res.render('shop/shopping-cart', {products : null})
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products : cart.generateArray(), totalPrice : cart.totalPrice})
+})
 
 module.exports = router;
